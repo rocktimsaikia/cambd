f1c2849b6f3cf535be61eaa89f051f34993efbfd
Rocktim Saikia <rocktimthedev@gmail.com>
2022-11-06 14:12:33 +0530

    refactor: pretty print, add examples and work type

--- cli.py
+++ cli.py
@@ -1,17 +1,18 @@
 #!/bin/env python3
 
 import sys
-from types import NoneType
 import requests
 from bs4 import BeautifulSoup
 from simple_term_menu import TerminalMenu
+import json
+
 
 spellcheck_url = "https://dictionary.cambridge.org/spellcheck/english/?q="
 definition_url = "https://dictionary.cambridge.org/dictionary/english/"
 headers = {"User-Agent": "Mozilla/5.0"}
 
 
-def get_suggestions(word):
+def get_suggestions(word: str) -> list[str]:
     page = requests.get(spellcheck_url + word, headers=headers)
     soup = BeautifulSoup(page.content, "html5lib")
     suggested_words = []
@@ -25,36 +26,51 @@ def get_suggestions(word):
     return suggested_words
 
 
-def get_definition(word):
+def get_definitions(word: str) -> list[str]:
     response = requests.get(definition_url + word, headers=headers)
 
-    # If redirection detcted that means, word is not valid. So get the suggestions insetad
+    # If redirection detcted that means, word is not valid
     if response.history and response.history[0].status_code == 302:
-        return
+        return []
 
     soup = BeautifulSoup(response.content, "html5lib")
     definitions = []
 
-    divs = soup.find_all(attrs={"class": "ddef_d"})
-    for div in divs:
-        content = div.get_text()
-        definitions.append(content)
+    containers = soup.find_all(attrs={"class": "entry-body__el"})
+    # divs = soup.find_all(attrs={"class": "ddef_d"})
+    for div in containers:
+        word_type = div.find(attrs={"class": "dpos"}).get_text()
+        definition = div.find(attrs={"class": "ddef_d"}).get_text()
+        example_containers = div.find_all(attrs={"class": "examp"})
+        examples = []
+        for expl in example_containers:
+            examples.append(expl.get_text().strip())
+
+        definition = definition.strip()[:-1].capitalize()
+        definitions.append(
+            {"type": word_type, "definition": definition, "examples": examples}
+        )
 
     return definitions
 
 
 def main():
     arg = sys.argv[1:][0]
-    definition = get_definition(arg)
+    definitions = get_definitions(arg)
 
-    if definition is None:
+    if len(definitions) == 0:
         suggestions = get_suggestions(arg)
-        terminal_menu = TerminalMenu(suggestions)
-        menu_entry_index = terminal_menu.show()
-        selected_suggestion = suggestions[menu_entry_index]
 
-        final_definition = get_definition(selected_suggestion)
-        print(final_definition)
+        print(f"No definition found for: \033[1m{arg}\033[0m ❗")
+        terminal_menu = TerminalMenu(suggestions, title="Did you mean?")
+        menu_entry_index = terminal_menu.show()
+
+        if type(menu_entry_index) is int:
+            arg = suggestions[menu_entry_index]
+            definitions = get_definitions(arg)
+
+    print(f"Showig definition for: \033[1m{arg}\033[0m")
+    print(json.dumps(definitions, indent=3))
 
 
 if __name__ == "__main__":
