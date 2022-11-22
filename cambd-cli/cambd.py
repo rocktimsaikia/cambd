0208b9b8d252708f1d0e2aacf2d6e22c786459b4
Rocktim Saikia <rocktimthedev@gmail.com>
2022-11-23 02:16:25 +0530

    refactor: move the bash port python

--- cambd-cli/cambd.py
+++ cambd-cli/cambd.py
@@ -2,7 +2,6 @@
 
 import os
 import re
-import sys
 import json
 import requests
 from bs4 import BeautifulSoup
@@ -10,6 +9,7 @@ from simple_term_menu import TerminalMenu
 from halo import Halo
 from rich import print
 import sqlite3
+import click
 
 spinner = Halo(text="Loading", spinner="dots")
 
@@ -123,39 +123,58 @@ def print_definition(word, definition, is_last):
     print("\n=====*=====") if not is_last else print("\n")
 
 
-def main():
-    args = sys.argv[1:]
-    searced_word = args[0]
+def clear_cache(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    con.execute(""" DROP TABLE IF EXISTS words """)
+    print("Cleared all the cambd word cache.")
+    ctx.exit()
 
-    # handle options
-    options = {"show_all": False}
-    if len(args) > 1:
-        if args[1] == "--show-all":
-            options["show_all"] = True
 
-    searched_word_filtered = searced_word.strip().replace(" ", "-").lower()
+@click.command()
+@click.argument("word")
+@click.option(
+    "-a",
+    "--show-all",
+    is_flag=True,
+    default=False,
+    help="Show all the definitions of a word.",
+)
+@click.option(
+    "-c",
+    "--clean-cache",
+    is_flag=True,
+    callback=clear_cache,
+    expose_value=False,
+    is_eager=True,
+    help="Clear all the stored cache from system.",
+)
+def main(word: str, show_all: bool):
+    """Cambridge dictionary CLI app"""
 
-    # handle db creation
-    table = """ CREATE TABLE IF NOT EXISTS words (
+    # Handle db creation
+    sql_create_query = """ CREATE TABLE IF NOT EXISTS words (
     word PRIMARY KEY,
     definitions TEXT
     ); """
-    con.execute(table)
+    con.execute(sql_create_query)
 
-    definitions = get_definitions(searched_word_filtered)
+    # Main
+    word_filtered = word.strip().replace(" ", "-").lower()
+    definitions = get_definitions(word_filtered)
     is_from_suggestions = False
 
     if len(definitions) == 0:
-        suggestions = get_suggestions(searched_word_filtered)
+        suggestions = get_suggestions(word_filtered)
 
-        spinner.fail(f"No definition found for: \033[1m{searced_word}\033[0m")
+        spinner.fail(f"No definition found for: \033[1m{word}\033[0m")
         terminal_menu = TerminalMenu(suggestions, title="Did you mean?")
         menu_entry_index = terminal_menu.show()
 
         if type(menu_entry_index) is int:
             suggested_word = suggestions[menu_entry_index]
             definitions = get_definitions(suggested_word)
-            searched_word_filtered = suggested_word
+            word_filtered = suggested_word
             is_from_suggestions = True
 
     if len(definitions) == 0:
@@ -164,18 +183,16 @@ def main():
 
     # Only show this when the the word was selected from suggestion menu
     if is_from_suggestions:
-        spinner.succeed(
-            f"Showing definition of \033[1m{searched_word_filtered}\033[0m instead"
-        )
+        spinner.succeed(f"Showing definition of \033[1m{word_filtered}\033[0m instead")
 
-    for i in range(len(definitions)):
-        if not options["show_all"]:
-            print_definition(searched_word_filtered, definitions[0], True)
-            break
-        is_last = (i + 1) == len(definitions)
-        print_definition(searched_word_filtered, definitions[i], is_last)
+    if not show_all:
+        print_definition(word_filtered, definitions[0], True)
+    else:
+        for i in range(len(definitions)):
+            is_last = (i + 1) == len(definitions)
+            print_definition(word_filtered, definitions[i], is_last)
 
-    cache_it(searched_word_filtered, definitions)
+    cache_it(word_filtered, definitions)
 
 
 if __name__ == "__main__":
